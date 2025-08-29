7c7faf2866fd1088289fc092dcea23126e4e3e8a
mayurdhvajsingh <vaghelamayurdhvajs@gmail.com>
2025-08-29 16:59:16 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 15;   
+    int b = 7;    
+    int result;   
+
+    result = a - b; 
+
+    printf("Subtraction = %d", result);
+
+    return 0;
+}
